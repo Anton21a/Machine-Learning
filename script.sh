b543b02e72fff84c43f1326a10139fab9ba1e7a0
Anton21a <shpilman.1995@gmail.com>
2025-04-02 14:46:45 +0300

    Очистка истории и обновление

--- script.sh
+++ script.sh
@@ -2,11 +2,15 @@
 
 cd ~/Desktop/MA1y/Prediction_with_ML/Machine-Learning || { echo "Папка не найдена"; exit 1; }
 
-git pull origin main
+# Разрешаем вытягивать, даже если истории не связаны
+git pull origin main --allow-unrelated-histories
 
+# Добавляем все изменения
 git add .
 
+# Коммит с сообщением из аргумента или по умолчанию
 commit_message=${1:-"Update"}
 git commit -m "$commit_message"
 
-git push origin main
+# Пушим с принудительной перезаписью ветки
+git push --force origin main
